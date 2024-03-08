9c1ddec05e12f4ca287b7fade4472ba335bc226b
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-03-08 10:56:03 -0300

    Projeto ex 18

--- ex018/app.py
+++ ex018/app.py
@@ -1,4 +1,7 @@
 # Faça um programa que leia um angulo na tela o valor do seno, cosseno e tangente desse angulo. 
 import math
-hipotenusa = int(input('Digite um número  '))
+hipotenusa = int(input('Digite uma largura '))
 seno = math.sin
+cosseno = math.cos
+tangente = math.tan
+print('O valor de Seno é {}, Cosseno {} e tangente {}'.format(seno, cosseno, tangente))
