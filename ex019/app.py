f0235833ade88a63d133d695e15173ca0f5aba38
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-03-08 15:26:37 -0300

    exercio 19 em processo.

--- ex019/app.py
+++ ex019/app.py
@@ -1,6 +1,7 @@
-#  Um professor que sortear um dos seus quatro alunos para pagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido. 
+#  Um professor que sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido. 
 # O profesor que sortear um dos alunos para pagar o quadro. 
-nome = int(input('Digite seu nome '))
-return = nome 
-print ('O nome do sorteado é {}'.format())
-
+import random
+nome1 = int(input('Digite seu nome '))
+nome2 = int(input('Digite seu nome '))
+nome3 = int(input('Digite seu nome '))
+nome4 = int(input('Digite seu nome '))
