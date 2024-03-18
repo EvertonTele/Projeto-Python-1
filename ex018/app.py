36e0b51cfec79a317d5649cb9473b4fc95260ed4
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-03-18 15:33:44 -0300

    corrigindo exercicios

--- ex018/app.py
+++ ex018/app.py
@@ -1,8 +1,17 @@
 # Faça um programa que leia um angulo na tela o valor do seno, cosseno e tangente desse angulo. 
-import math
-hipotenusa = int(input('Digite uma largura '))
-seno = math.sin(hipotenusa)
-cosseno = math.cos(hipotenusa)
-tangente = math.tan(hipotenusa)
-print('O valor de Seno é {}, Cosseno {} e tangente {}'.format(seno, cosseno, tangente))
+# import math
+# hipotenusa = float(input('Digite uma largura '))
+# seno = math.sin(hipotenusa)
+# cosseno = math.cos(hipotenusa)
+# tangente = math.tan(hipotenusa)
+# print('O valor de Seno é {:.2f}, Cosseno {:.2f} e tangente {:.2f}'.format(seno, cosseno, tangente))
 
+# Reposta do exercicio 
+from math import radians, sin, cos, tan
+angulo = float(input('Digite o angulo'))
+seno = sin(radians(angulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+cosseno = cos(radians(angulo))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+tangente = tan(radians(angulo))
+print('O ângulo de {} tem o TANGENTE {:.2f}'.format(angulo, tangente))
