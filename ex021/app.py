3b69ad57537bea4a5c5ad06f9e71329792d53bb3
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-03-21 13:36:57 -0300

    correção dos ex

--- ex021/app.py
+++ ex021/app.py
@@ -1,3 +1,15 @@
 #  Faça um programa em python que abra e reproduza o áudio de um arquivo MP3
+# import pygame
+# playsound.playsound('MP3.mp3')
+
+
+
+
+# resposta do exercicio 
 import pygame
-playsound.playsound('MP3.mp3')
+pygame.init()
+pygame.mixer.music.load('ex0021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+
