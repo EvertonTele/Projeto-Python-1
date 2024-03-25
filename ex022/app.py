b38393ade252cb7c6849bdd0874967bee6979b7b
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-03-25 15:07:22 -0300

    Corrigindo ex 22

--- ex022/app.py
+++ ex022/app.py
@@ -11,3 +11,12 @@ print(len(nome.strip()))
 dividindo = nome.split()
 print(dividindo[0])
 
+# Correção do exercicio 
+# nome = str(input('Digite seu nome completo ')).strip()
+# print('Analisando seu nome... ' )
+# print('Seu nome em maiúsculo é {}'.format(nome.upper()))
+# print('Seu nome em minúsculo é{}'.format(nome.lower()))
+# print('Seu nome tem ao todo {} letras '.format(len(nome) - nome.count('')))
+# # print ( 'Seu primeiro nome tem{} letras'. format(nome.find('')))
+# separa = nome.split
+# print('Seu primeiro nome é {} e ele tem {} letras'. format(separa[0], len(separa)))
