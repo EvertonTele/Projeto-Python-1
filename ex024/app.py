7aad6065948e6ba43540dbc1efd7a95b7b786869
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-04-03 12:23:54 -0300

    Resumo ex

--- ex024/app.py
+++ ex024/app.py
@@ -1,6 +1,12 @@
 # Crie um programa que leia o nome de uma Cidade  e diga se ela começa ou não com a palavra "Santos".
 
-cidade = str(input('Digite o nome da Cidade '))
+cidade = str(input('Que cidade você nasceu? '))
 cidade1 = str(cidade)
 print('Santos'in cidade)
+
+
+# # resposta exercicio:
+# cid = str(input('Em que cidade você nasceu? ')).strip()
+# print(cid[:5].upper() == 'SANTO')
+
 
