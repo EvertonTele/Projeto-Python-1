f8a9c836066ed07f0eab05de537964c841b43e0b
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-09 07:46:14 -0300

    continuação dos ex

---
+++ ex041/app.py
@@ -0,0 +1,7 @@
+# Aconfederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade: 
+
+# - Até 9 anos: Mirim
+# - A´te 14 anos: Infantil 
+# - Até 19 anos: Junio
+# - Até 20 anos: Sênior 
+# - Acima: Master. 
---
+++ ex042/app.py
@@ -0,0 +1,5 @@
+# Refaça o Desafio 035 dos triângulos acrecentando o recurso de mostrar que tipo de riâmgulo será formado:
+
+# - Equilátero: Todos os lados iguais 
+# - Isósceles=: dois lados iguais 
+# - Escaleno: Todos os lados direferentes. 
