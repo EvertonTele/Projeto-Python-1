00dcd408e7e01a148e274a67959b5ea2f7beab0e
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-13 16:46:54 -0300

    ex

--- ex029/app.py
+++ ex029/app.py
@@ -9,7 +9,8 @@ valor_multa = (Velocidade - 80) * 7.00
 if Velocidade > 80:
     print('Você foi multado')
     print('A multa custa R$7,00 por KM acima do limite. ')
-    print('O valor da multa é R${:.2f}'.format(valor_multa))
+    # print('O valor da multa é R${:.2f}'.format(valor_multa))
+    print(f'O valor da multa é R${valor_multa:.2f}')
 else:
     print('Voce não foi multado...')
 
--- ex036/app.py
+++ ex036/app.py
@@ -1,3 +1,17 @@
 # Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar. 
 
 # Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+# valor_do_imovel = int(input('Qual o valor da sua casa?'))
+# salario = int(input('Digite seu salário'))
+# # prazo = int(input('Prazo de Pagamento'))
+
+imovel = float(input('Qual o valor da sua casa?'))
+salario = float(input('Digite seu salário'))
+prazo = float(input('Prazo de Pagamento'))
+parcela =  imovel / prazo
+prestacao_mensal = salario + salario *0.30
+if parcela >= prestacao_mensal:
+    print(f'a parcela não pode exceder 30 % do seu salário, valor da parcela {parcela:.2f}')
+else:
+    print(f' valor da parcela é de {parcela:.2f}')
