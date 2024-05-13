17e8a6ef6c67d8f5e812024414b92caf3df06c89
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-13 17:12:39 -0300

    ex

--- ex037/app.py
+++ ex037/app.py
@@ -4,3 +4,16 @@
 # - 2 para octal 
 # - 3 para hexadecimal
 
+numero_conversão = float(input('Digite um número: '))
+print('Escolha a base de conversão')
+print('1 - Binário ')
+print('2 - Octal ')
+print('3 - Hexadecimal ')
+Opcao = str('Digite a opção desejada')
+
+if Opcao == 1:
+    print('O número é binário', bin(numero_conversão))
+elif Opcao == 2:
+    print('é um número octal! ', oct(numero_conversão))
+elif Opcao == 3:
+    print ('É um núemro exadecimal! ', hex(numero_conversão))
