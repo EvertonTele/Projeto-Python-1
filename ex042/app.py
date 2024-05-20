04c29fcdc321fcd761abe469058094ed397a4e49
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-20 11:49:22 -0300

    ex

--- ex042/app.py
+++ ex042/app.py
@@ -3,3 +3,16 @@
 # - Equilátero: Todos os lados iguais 
 # - Isósceles=: dois lados iguais 
 # - Escaleno: Todos os lados direferentes. 
+
+print('-='*20)
+print('Analisador de Triaângulos ')
+print('-='*20)
+r1 = float(input('Primeiro segmento:'))
+r2 = float(input('Segundo segmento:'))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('O triângulo é um Equilatero, todos os lados são iguais.  ')
+elif r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('O triângylo é um Isóceles dois lados apenas são iguais. ')
+else:
+    print('O triangulo não foi formado é um Escaleno!  ')
