c407528cb8154fb4004b9cfaedf843d6b13f7a8f
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-23 10:07:16 -0300

    ex

--- ex043/app.py
+++ ex043/app.py
@@ -5,3 +5,19 @@
 # - 25 até 30: Sobrepeso 
 # - 30 até 40: Obesidade 
 # - Acima de 40: Obesidade mórbida
+
+Peso = float(input('Digite o Peso: '))
+Altura = float(input('Digite a Altura '))
+imc = Peso/(Altura*Altura)
+print('Seu IMC é {:.2f}'.format(imc))
+if imc > 40:
+    print('Você está com Obesidade Mórbida! ')
+elif 30< imc <=40:
+    print('Você está Obeso! ') 
+elif 25 < imc <=30:
+    print('Você está com Sobre peso! ')
+elif 18.5 < imc <=25:
+    print('Você está com o Peso Ideal! ')
+else:
+    print('Você está abaixo do peso! ')
+    
