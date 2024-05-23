9042573003d220a329710bd955682b7600e56011
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-05-23 16:18:30 -0300

    ex

--- ex044/app.py
+++ ex044/app.py
@@ -4,3 +4,29 @@
 # - Á vista no cartão: 5% de desconto 
 # - em até 2x no cartão: preço normal 
 # - 3x ou mais no cartão 20% de juros. 
+
+Valor = float(input('Digite o valor do produto: '))
+print('Escola a forma de pagamento! ')
+print('1. Valor a vista dinheiro/ Cheque: 10(%) de desconto! ')
+print('2. Valor á vista no cartão: 5(%) de desconto! ')
+print('3.Valor dividido em até 2x no cartão: Preçop normal! ')
+print('4. Valor 3x ou mais no cartão 20(%) de juros! ')
+Opcao = int(input('Escolha a opção de 1-4: '))
+if  Opcao ==1:
+    Valor_final = Valor *0.90
+    print('Você escolheu a forma á vista sua forma de pagamento, o valor cobrado será de R${}'.format(Valor_final))
+elif Opcao == 2:
+    valor_final = Valor*0.95
+    print('Você escolheu a forma de pagamento á vista no cartão, o valor a ser cobrado é de R${}'.format(valor_final))
+elif Opcao == 3:
+    valor_final = Valor
+    print('Você escolheu dividir em até 2x no cartão seu valor é de R${}'.format(valor_final))
+else:
+    valor_final = Valor* 1.20
+    print('Você escolheu a forma de pagamento acima de 3x no cartão, o valor a ser cobrado é de R${}'.format(valor_final))
+
+# Avista = (10/100 )* Valor_poduto
+# avista_cartão = (5/100)*Valor_poduto
+# sem_descont = 1*Valor_poduto
+# cartao3x = 0.20*Valor_poduto
+
