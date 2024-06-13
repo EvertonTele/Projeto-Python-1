2f83729669aa327e979d4091d59cd9cedc1317a0
Everton dos Santos Teles <139225703+EvertonTele@users.noreply.github.com>
2024-06-13 09:29:41 -0300

    ex

--- ex051/app.py
+++ ex051/app.py
@@ -1,1 +1,7 @@
-# Desenvolva um programa que leia o preimeiro termo e a razão de um PA. no final, mostre os 10 primeiros termos dessa progressão.
+# Desenvolva um programa que leia o primeiro termo e a razão de um PA. no final, mostre os 10 primeiros termos dessa progressão.
+num = int(input('Digite uma PA: '))
+raz = int(input('Digite uma RAZÂO: '))
+print('Os 10 primeiro temos da PA são: ')
+for c in range(10):
+    termo = num + c * raz
+    print(termo)
